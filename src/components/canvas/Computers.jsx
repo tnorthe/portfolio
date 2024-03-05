1a0a48d87593d93649a922d791d8c28b9b36e1a6
Tamara <tamara.northe@laparkan.com>
2024-03-05 11:06:08 -0400

    changes made to Computer file to render 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,24 +1,43 @@
-import {Suspense, useEffect, useState} from 'react';
+import {Suspense, useRef, useEffect, useState} from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
+import { HemisphereLight, PointLight } from 'three';
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
+  const { nodes, materials } = useGLTF('./desktop_pc/scene.gltf');
+  console.log('Nodes:', nodes);
+  console.log('Materials:', materials);
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black"/>
-      <pointLight intensity={1} />
-      <primitive
-        object={computer.scene}
-      />
-    </mesh>
+    <group>
+      <mesh>
+      <primitive object={computer.scene} scale={0.1} position={[0, 0, 0]} />
+      </mesh>
+    </group>
   )
 }
 
 const ComputersCanvas = () => {
   return (
-    <Canvas></Canvas>
+    <Canvas
+      frameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{preserveDrawingBuffer: true}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <HemisphereLight intensity={0.15} groundColor="black"/>
+        <PointLight intensity={1} />
+        <Computers /> 
+      </Suspense> 
+      <Preload all />
+    </Canvas>
   )
 }
 
