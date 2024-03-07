a1195cc6e76d5d83bcb4cb7265c08b55b1de8b83
Tamara <tamara.northe@laparkan.com>
2024-03-07 09:29:24 -0400

    made dynamic cards to show the various skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,23 @@ import {fadeIn, textVariant} from '../utils/motion';
 
 const ServiceCard = ({index, title, icon}) => {
   return (
-    <p>{title}</p>
+    <Tilt className="xs:w-[250px] w-full"
+    options={{
+      max: 45,
+      scale: 1,
+      speed: 450
+    }}
+    >
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+          <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
