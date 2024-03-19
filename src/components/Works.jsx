8c32d18a342fa661970991857af6354e684c24ac
Tamara <tamara.northe@laparkan.com>
2024-03-19 09:52:10 -0400

    started creating the works setion of page

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({ index, title, description, tech, github, demo }) => {
+  
+}
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Following are some of the projects I have worked on. You can find more of my work on my GitHub profile.
+          Each project has a link to the GitHub repository and a link to the live demo if available.
+          It reflects my skills and expertise in web development and software engineering.
+        </motion.p>
+      </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
