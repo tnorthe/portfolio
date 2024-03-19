06ae95383298c02305623182cce6e015ecd15f22
Tamara <tamara.northe@laparkan.com>
2024-03-19 09:23:36 -0400

    changes made to Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,35 +1,40 @@
-import React, {Suspense} from 'react';
-import { Canvas } from '@react-three/fiber';
-import {Decal, OrbitControls, Preload, Float, useTexture} from '@react-three/drei';
+import React, {Suspense, useRef} from 'react';
+import * as THREE from 'three';
+import { Canvas, useFrame } from '@react-three/fiber';
+import {Decal, OrbitControls, Preload, Float, useTexture, Icosahedron} from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 import PropTypes from 'prop-types';
 
 const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+  //const [decal] = useTexture([props.imgUrl]);
+  const light = useRef();
+
+  useFrame(() => {
+    if (light.current) {
+      light.current.position.set(0, 0, 0.05);
+    }
+  });
+
+  // Check if decal is a valid texture
   
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={1.75} />
-      <directionalLight position={[0,0,0.05]}/>
+      <ambientLight intensity={0.5} />
+      <directionalLight ref={light}/>
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
+        <Icosahedron args={[1, 1]} />
         <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading/>
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          map={decal}
-        />  
       </mesh>
     </Float>
   )
 }
 
-Ball.propTypes = {
+/*Ball.propTypes = {
   imgUrl: PropTypes.string.isRequired,
-};
+};*/
 
-const BallCanvas = ({icon}) => {
+const BallCanvas = ({ icon }) => {
   return (
     <Canvas
       frameLoop="demand"
@@ -37,11 +42,15 @@ const BallCanvas = ({icon}) => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false}/>
-        <Ball imgUrl={icon} /> 
+        <Ball imgUrl={icon} />
       </Suspense> 
       <Preload all />
     </Canvas>
   )
 }
 
-export default BallCanvas
+/*BallCanvas.propTypes = {
+  icon: PropTypes.string.isRequired,
+};*/
+
+export default BallCanvas;
