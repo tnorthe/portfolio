f7e5b1908c03315c02d5a8945c182e873efb6dfe
Tamara <tamara.northe@laparkan.com>
2024-04-11 13:07:31 -0400

    added new logo to portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,21 @@ import { Link } from "react-router-dom";
 
 import {styles} from "../styles";
 import {navLinks} from "../constants";
-import {logo, menu, close} from "../assets";
+import {menu, close} from "../assets";
+import logo from '../assets/t-logo.png';
 
 const Navbar = () => {
   const [active, setActive] = useState('');
   const [toggle, setToggle] = useState(false);
   return (
-    <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-10 bg-primary bg-opacity-90`}
-    >
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-10 bg-primary bg-opacity-90`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
           to="/"
           className='flex items-center gap-2'
           onClick={() => {
-            setActive('')
-            window.scrollTo(0, 0)  
+            setActive('');
+            window.scrollTo(0, 0);  
           }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
