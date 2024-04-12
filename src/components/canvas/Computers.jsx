accf963fd3eed57f0662bb11954aaeb3c0a42b22
Tamara <tamara.northe@laparkan.com>
2024-04-12 09:42:00 -0400

    made adjustments to computer canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,8 @@
-import {Suspense, useRef, useEffect, useState} from 'react';
+import {Suspense, useEffect, useState} from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
-import { HemisphereLight, PointLight } from 'three';
+
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
@@ -10,11 +10,23 @@ const Computers = () => {
   console.log('Nodes:', nodes);
   console.log('Materials:', materials);
   return (
-    <group>
-      <mesh>
-      <primitive object={computer.scene} scale={0.1} position={[0, 0, 0]} />
-      </mesh>
-    </group>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black"/>
+      <pointLight intensity={1} />
+      <spotLight 
+        position={[-20, 50, 10]} 
+        angle={0.12} 
+        penumbra={1}
+        intensity={1} 
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive 
+        object={computer.scene} 
+        scale={[0.75]}
+        position={[0,-3.25,-1.5]} 
+        rotation={[-0.01,-0.2,-0.1]}/>
+    </mesh>
   )
 }
 
@@ -32,8 +44,6 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <HemisphereLight intensity={0.15} groundColor="black"/>
-        <PointLight intensity={1} />
         <Computers /> 
       </Suspense> 
       <Preload all />
