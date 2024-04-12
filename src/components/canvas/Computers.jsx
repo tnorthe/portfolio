18e2df981c48c9780578f660d3ac02853046ebb8
Tamara <tamara.northe@laparkan.com>
2024-04-12 10:14:50 -0400

    modifed computers canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -23,7 +23,7 @@ const Computers = () => {
       />
       <primitive 
         object={computer.scene} 
-        scale={[0.75]}
+        scale={0.75}
         position={[0,-3.25,-1.5]} 
         rotation={[-0.01,-0.2,-0.1]}/>
     </mesh>
@@ -51,4 +51,4 @@ const ComputersCanvas = () => {
   )
 }
 
-export default Computers
+export default ComputersCanvas;
