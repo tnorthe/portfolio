7baaf908dcb743b3e6b8ca9e33e6f0a8edc94474
Tamara <tamara.northe@laparkan.com>
2024-04-24 09:57:33 -0400

    made stars visblle in the contact section

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,7 +6,7 @@ import * as random from 'maath/random/dist/maath-random.esm';
 const Stars = (props) => {
   const ref = useRef();
 
-  const sphere = random.inSphere(new Float32Array(500),{radius: 1.2});
+  const sphere = random.inSphere(new Float32Array(5000),{radius: 1.2});
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta /10;
@@ -14,7 +14,7 @@ const Stars = (props) => {
   })
 
   return (
-    <group>
+    <group rotation={[0, 0, Math.PI/4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
