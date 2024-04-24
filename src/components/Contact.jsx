3813451b1bb5c52a5966988165cf2e1bbfd7c9db
Tamara <tamara.northe@laparkan.com>
2024-04-24 09:57:58 -0400

    uncommented some lines

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -113,10 +113,10 @@ const Contact = () => {
         </form>
       </motion.div>
       <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
+        variants={slideIn("right", "tween", 0.2, 1)}
         className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
-        {/*<EarthCanvas />*/}
+        <EarthCanvas />
       </motion.div>
     </div>
   )
